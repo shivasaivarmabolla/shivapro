c36fc56af790d988c9f94ce013be5cc966a85461
shivasaivarmabolla <111185190+shivasaivarmabolla@users.noreply.github.com>
2022-08-13 21:32:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import joblib
 
 #load the joblib model
-model_nb = joblib.load('spam-ham(1)')
+model_nb = joblib.load('spam-ham)
 
 st.title('SPAM HAM CLASSIFIER')
 ip = st.text_input('Enter your text :')
